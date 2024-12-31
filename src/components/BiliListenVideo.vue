084072b5c8bde929f0d91ee6234525ebfc433014
Liu Yuhe <171144077+liuyuhe666@users.noreply.github.com>
2024-12-31 12:28:35 +0800

    refactor: update

--- src/components/BiliListenVideo.vue
+++ src/components/BiliListenVideo.vue
@@ -3,12 +3,6 @@ import { ref } from 'vue'
 
 const bvid = ref('')
 const audioUrl = ref('')
-const picUrl = ref('')
-const showControlPanel = ref(false)
-
-function toggleControlPanel() {
-  showControlPanel.value = !showControlPanel.value
-}
 
 async function getVideoInfo(bvid: string) {
   if (bvid) {
@@ -29,8 +23,7 @@ async function getVideoStream(bvid: string, cid: string) {
 async function handleClick() {
   const videoInfo = await getVideoInfo(bvid.value)
   if (videoInfo) {
-    const { cid = '', pic = '' } = videoInfo
-    picUrl.value = pic
+    const { cid = '' } = videoInfo
     const videoStream = await getVideoStream(bvid.value, cid)
     const { dash = {} } = videoStream
     if (dash) {
@@ -45,48 +38,25 @@ async function handleClick() {
 </script>
 
 <template>
-  <div class="bili-listen-video-container">
-    <h2>哔哩哔哩 (゜-゜)つロ 干杯~</h2>
-    <div>
-      <input v-model="bvid" type="text" placeholder="请输入视频 bvid" class="bili-listen-video-input">
-      <button type="button" class="bili-listen-video-button" @click="handleClick">
-        听视频
-      </button>
-    </div>
-    <div v-if="audioUrl.length" class="video-container" @click="toggleControlPanel">
-      <video :controls="showControlPanel" autoplay loop :src="audioUrl" :poster="picUrl" />
+  <div class="h-full bg-[url('/bg.webp')] bg-cover bg-center">
+    <div class="container mx-auto h-full flex items-center justify-center flex-col">
+      <div class="p-2">
+        <h2 class="text-2xl font-semibold py-4 text-white">
+          哔哩哔哩 (゜-゜)つロ 干杯~
+        </h2>
+        <div class="flex flex-row justify-between items-center">
+          <input v-model="bvid" type="text" placeholder="请输入视频的 bvid" class="text-base font-medium rounded-lg p-3 bg-gray-100">
+          <button type="button" class="text-base font-medium rounded-lg p-3 bg-sky-500 text-white" @click="handleClick">
+            听视频
+          </button>
+        </div>
+      </div>
+      <div v-if="audioUrl.length" class="p-2">
+        <audio controls autoplay loop :src="audioUrl" />
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.bili-listen-video-container {
-  display: flex;
-  gap: 20px;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-.bili-listen-video-container h2 {
-  color: #fff;
-}
-.bili-listen-video-input {
-  padding: 8px;
-  margin-right: 8px;
-}
-.bili-listen-video-button {
-  padding: 8px;
-  background-color: #007AFF;
-  color: #fff;
-}
-.video-container {
-  width: 960px;
-  height: 540px;
-}
-
-.video-container video {
-  width: 100%;
-  height: 100%;
-  object-fit: fill;
-}
 </style>
